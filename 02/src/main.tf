9fa8025e48204710dccd47783d759f0ed697d809
mrparfentyev <mr.parfentyev@gmail.com>
2026-01-08 19:34:55 +0300

    Выполнил задание 5 введения в тераформ

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -13,7 +13,7 @@ data "yandex_compute_image" "ubuntu" {
   family = var.vm_web_image_family
 }
 resource "yandex_compute_instance" "platform" {
-  name        = var.vm_web_name
+  name        = local.vm_web_name
   platform_id = var.vm_web_platform_id
   resources {
     cores         = var.vm_web_cores
@@ -55,7 +55,7 @@ resource "yandex_vpc_subnet" "develop_b" {
 }
 
 resource "yandex_compute_instance" "platform_db" {
-  name        = var.vm_db_name
+  name        = local.vm_db_name
   platform_id = var.vm_db_platform_id
   zone        = var.vm_db_zone
 
